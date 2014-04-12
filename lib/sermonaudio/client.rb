001ca172ac197be1f495762854290806a6267c35
Cody Barr <cody.barr@gmail.com>
2014-04-12 15:38:08 -0400

    Updated client.rb for latest Savon compatability

--- lib/sermonaudio/client.rb
+++ lib/sermonaudio/client.rb
@@ -6,18 +6,9 @@ module SermonAudio
 
     extend self
     
-    ## Set base configuration
-    Savon.configure do |config|
-      config.log = false            # disable logging
-      config.log_level = :error     # changing the log level
-      config.logger = false
-      config.raise_errors = false   # do not raise SOAP faults and HTTP errors
-      config.soap_version = 2       # use SOAP 1.2
-    end
-    
     ## Setup the client
     def client      
-      @sermonaudio_savon_client ||= Savon::Client.new 'http://web4.sa-media.com/SASoapAPI/service.asmx?WSDL'
+      @sermonaudio_savon_client ||= Savon:.client(wsdl: 'http://web4.sa-media.com/SASoapAPI/service.asmx?WSDL')
     end
     
   end
